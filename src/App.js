55c6fcb2ef503413db773dea726c330807eb462f
Pulkit Garg <pulkitgarg360@gmail.com>
2022-06-05 14:19:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Home from './components/Home';
+import CollectClinicals from './components/CollectClinicals';
+import AddPatient from './components/AddPatient';
+import AnalyseData from './components/AnalyseData';
+import ChartGenerator from './components/ChartGenerator';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/patientDetails/:patientId" component={CollectClinicals}/>
+          <Route exact path="/addPatient" component={AddPatient}/>
+          <Route exact path="/analyse/:patientId" component={AnalyseData}/>
+          <Route exact path="/chart/:componentName/:patientId" component={ChartGenerator}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/AnalyseData.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class AnalyseData extends React.Component {
+    render() {
+        return(<div>
+
+        </div>)
+    }
+}
+
+export default AnalyseData;
